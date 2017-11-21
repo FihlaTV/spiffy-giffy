7e8a69830853c0898e4fe2f8bc7037fa1adc8e15
Katy <ihatetoast@gmail.com>
2017-11-21 14:32:13 -0600

    buttons loaded from array. some bootstrap layout

---
+++ README.md
@@ -0,0 +1,19 @@
+# Spiffy with Giphy
+A small project to practice AJAX GET requests using the Giphy API.
+
+
+# INSTRUCTIONS
+* (__) create an array of strings of topics i like (let topics)
+* (__) iterate over topics and create buttons in your HTML.
+* (__) <button> behaviour: 
+    - get 10 static/non-animated imgs from GIPHY API
+    - append to the page
+    - rating below the img
+* (__) <img> behaviour:
+    - click to animate / stop animate
+    - toggle
+* (__) Create a form element
+    - input field
+    - push value to array
+    - append to div with new button.
+* ensure http https is dealt with to ensure smooth deployment
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,49 @@
 $(document).ready(function(){
 console.log("if you see this, you dun did load.");
 
+let topics = ["greyhound", "dog", "cat", "kitten", "puppy", "sloth", "cycling", "buscemi", "motorcycle", "knitting", "australia"];
+
+// dynamically generates buttons
+let giphrames = topics.map((topic, idx, arr) =>{
+	// console.log(idx, topic);
+	$('.buttonHolder').append(`
+		<button class="btn btn-primary btn-sm topic" id=${topic}>${topic}</button>
+	`);
 });
+
+$('.topic').on("click", function(){
+	console.log($(this).attr("id"));
+	// console.log(`${$(this).id} button booped`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+// GIF FRAME
+
+// <div class="row gifHolder">
+// 	<div class="col-xs-5 col-sm-3 col-md-3 col-lg-3">
+// 		<img src="" alt=""/>
+// 		<caption class="rating"></caption>
+// 	</div>
+// </div>
